8eb6a625a7cfcc3aaa12d5cb9f9362a306c19724
Ilya Tronenko <BigWander@mail.ru>
2023-12-21 16:36:03 +0700

    Print response on main screen

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -5,9 +5,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -15,6 +17,7 @@ private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment: Fragment() {
     private lateinit var photoRecyclerView: RecyclerView
+    private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,13 +31,41 @@ class PhotoGalleryFragment: Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view,
+            savedInstanceState)
+        photoGalleryViewModel.galleryItemLiveData.observe(
+            viewLifecycleOwner,
+            Observer { galleryItems ->photoRecyclerView.adapter = PhotoAdapter(galleryItems)
+            }) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val flickrLiveData: LiveData<List<GalleryItem>> = FlickFetchr().fetchPhotos()
-        flickrLiveData.observe(
-            this,
-            Observer { galleryItems -> Log.d(TAG, "Response received:$galleryItems")
-            })
+        photoGalleryViewModel = ViewModelProviders.of(this)[PhotoGalleryViewModel::class.java]
+    }
+
+    private class PhotoHolder(itemTextView: TextView)
+        : RecyclerView.ViewHolder(itemTextView)
+    {
+        val bindTitle: (CharSequence) -> Unit = itemTextView::setText
+    }
+
+    private class PhotoAdapter(private val galleryItems: List<GalleryItem>)
+        : RecyclerView.Adapter<PhotoHolder>() {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): PhotoHolder {
+            val textView =
+                TextView(parent.context)
+            return PhotoHolder(textView)
+        }
+        override fun getItemCount(): Int = galleryItems.size
+        override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
+            val galleryItem =
+                galleryItems[position]
+            holder.bindTitle(galleryItem.title)
+        }
     }
 
     companion object {
