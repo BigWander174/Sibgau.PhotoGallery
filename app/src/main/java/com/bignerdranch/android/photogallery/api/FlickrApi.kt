939331a5e2c0034d3d2bc3a4e2fe414e65b6f718
Ilya Tronenko <BigWander@mail.ru>
2023-12-21 16:27:44 +0700

    Add serialization

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -1,9 +1,16 @@
 package com.bignerdranch.android.photogallery.api
 
+import com.bignerdranch.android.photogallery.FlickrResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface FlickrApi {
-    @GET("/")
-    fun fetchContents(): Call<String>
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+        "&api_key=c03bcd2a0de4a0b735d6a13679a4753a" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<FlickrResponse>
 }
